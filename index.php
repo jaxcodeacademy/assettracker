75a6c4b71ba0a119d2b6b4573588a0f577194cc6
Stephen Fitzmeyer MD <support@jaxcode.com>
2020-08-12 14:34:32 +0000

    new files

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <!DOCTYPE html>
+<html>
+<head>
+<title>Asset Tracker App</title>
+</head>
+<body>
+<h1>Asset Tracker App</h1>
+<form>
+Asset Name: <input type="text" name="assetname"><br>
+
+</form>
+
+
+</body>
+</html>
